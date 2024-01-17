13f448d43baa5ff3b06c1d57f8fbf33d106a70b4
tammy.workspace <tammy.workspace>
2024-01-17 16:47:40 +0000

    do not re-download the data and add hover_data

--- app.py
+++ app.py
@@ -28,8 +28,9 @@ variable_dropdown = dcc.Dropdown(
     options=df.columns, value='BARO_PRES_MEAN', multi=False, id='variable_dropdown-component')
 plot_value = Rv(variable_dropdown, 'value')
 map = px.scatter_geo(df, lat='latitude', lon='longitude', color=plot_value,
-                     fitbounds='locations', height=750, color_continuous_scale=px.colors.sequential.Inferno)
-figure = px.line(df, x='time', y=plot_value, color='drone')
+                     fitbounds='locations', height=750, color_continuous_scale=px.colors.sequential.Inferno,
+                      hover_data=['drone', 'latitude', 'longitude', 'time', plot_value])
+figure = px.line(df, x='time', y=plot_value, color='drone',  hover_data=['drone', 'latitude', 'longitude', 'time', plot_value])
 figure.update_yaxes(title=dict(text=plot_value))
 figure.update_layout(title=plot_value)
 
@@ -178,34 +179,35 @@ app.layout = html.Div([
 
 @callback(Output("map-component", "figure"), Input("variable_dropdown-component", "value"), prevent_initial_call=True)
 def update(plot_value):
-    u1040 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1040_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
-    u1041 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1041_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
-    u1068 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1068_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
-    d1040 = pd.read_csv(u1040, skiprows=[1])
-    d1040['drone'] = 'sd1040'
-    d1041 = pd.read_csv(u1041, skiprows=[1])
-    d1041['drone'] = 'sd1041'
-    d1068 = pd.read_csv(u1068, skiprows=[1])
-    d1068['drone'] = 'sd1068'
-    df = pd.concat([d1040, d1041, d1068])
+    # u1040 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1040_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
+    # u1041 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1041_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
+    # u1068 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1068_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
+    # d1040 = pd.read_csv(u1040, skiprows=[1])
+    # d1040['drone'] = 'sd1040'
+    # d1041 = pd.read_csv(u1041, skiprows=[1])
+    # d1041['drone'] = 'sd1041'
+    # d1068 = pd.read_csv(u1068, skiprows=[1])
+    # d1068['drone'] = 'sd1068'
+    # df = pd.concat([d1040, d1041, d1068])
     map = px.scatter_geo(df, lat='latitude', lon='longitude', color=plot_value,
-                         fitbounds='locations', height=750, color_continuous_scale=px.colors.sequential.Inferno)
+                         fitbounds='locations', height=750, color_continuous_scale=px.colors.sequential.Inferno,
+                         hover_data=['drone', 'latitude', 'longitude', 'time', plot_value])
     return map
 
 
 @callback(Output("figure-component", "figure"), Input("variable_dropdown-component", "value"), prevent_initial_call=True)
 def update(plot_value):
-    u1040 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1040_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
-    u1041 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1041_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
-    u1068 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1068_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
-    d1040 = pd.read_csv(u1040, skiprows=[1])
-    d1040['drone'] = 'sd1040'
-    d1041 = pd.read_csv(u1041, skiprows=[1])
-    d1041['drone'] = 'sd1041'
-    d1068 = pd.read_csv(u1068, skiprows=[1])
-    d1068['drone'] = 'sd1068'
-    df = pd.concat([d1040, d1041, d1068])
-    figure = px.line(df, x='time', y=plot_value, color='drone')
+    # u1040 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1040_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
+    # u1041 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1041_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
+    # u1068 = 'https://data.pmel.noaa.gov/generic/erddap/tabledap/sd1068_hurricane_2023.csv?time,latitude,longitude,BARO_PRES_MEAN,WIND_SPEED_MEAN,TEMP_SBE37_MEAN,WAVE_SIGNIFICANT_HEIGHT&time>=2023-10-21&time<=2023-10-25'
+    # d1040 = pd.read_csv(u1040, skiprows=[1])
+    # d1040['drone'] = 'sd1040'
+    # d1041 = pd.read_csv(u1041, skiprows=[1])
+    # d1041['drone'] = 'sd1041'
+    # d1068 = pd.read_csv(u1068, skiprows=[1])
+    # d1068['drone'] = 'sd1068'
+    # df = pd.concat([d1040, d1041, d1068])
+    figure = px.line(df, x='time', y=plot_value, color='drone', hover_data=['drone', 'latitude', 'longitude', 'time', plot_value])
     figure.update_yaxes(title=dict(text=plot_value))
     figure.update_layout(title=plot_value)
     return figure
